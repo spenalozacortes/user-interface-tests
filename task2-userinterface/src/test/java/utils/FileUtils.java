3aeee87f60ce3e243ca7f7f4264201a60fc3754d
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-11-26 18:29:20 -0600

    Handle AWTException

--- task2-userinterface/src/test/java/utils/FileUtils.java
+++ task2-userinterface/src/test/java/utils/FileUtils.java
@@ -7,20 +7,25 @@ import java.io.File;
 
 public class FileUtils {
 
-    private static final int delay = 3000;
+    private static final int DELAY = 3000;
 
-    public static void handleFileDialog(String path) throws AWTException {
-        Robot robot = new Robot();
-        String filePath = new File(path).getAbsolutePath();
-        StringSelection stringSelection = new StringSelection(filePath);
-        robot.delay(delay);
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+    public static void handleFileDialog(String path) {
+        try {
+            Robot robot = new Robot();
+            String filePath = new File(path).getAbsolutePath();
+            StringSelection stringSelection = new StringSelection(filePath);
+            robot.delay(DELAY);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
 
-        robot.keyPress(KeyEvent.VK_CONTROL);
-        robot.keyPress(KeyEvent.VK_V);
-        robot.keyRelease(KeyEvent.VK_CONTROL);
-        robot.keyRelease(KeyEvent.VK_V);
-        robot.keyPress(KeyEvent.VK_ENTER);
-        robot.keyRelease(KeyEvent.VK_ENTER);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        } catch (AWTException e) {
+            System.err.println("Error handling file dialog: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
