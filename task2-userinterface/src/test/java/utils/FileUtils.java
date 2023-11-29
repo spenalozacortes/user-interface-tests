7b48221f940db672679039bfe881ceccaf5ec2e2
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-11-29 13:07:06 -0600

    Add robot delay

--- task2-userinterface/src/test/java/utils/FileUtils.java
+++ task2-userinterface/src/test/java/utils/FileUtils.java
@@ -16,11 +16,11 @@ public class FileUtils {
             StringSelection stringSelection = new StringSelection(filePath);
             robot.delay(DELAY);
             Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
-
             robot.keyPress(KeyEvent.VK_CONTROL);
             robot.keyPress(KeyEvent.VK_V);
             robot.keyRelease(KeyEvent.VK_CONTROL);
             robot.keyRelease(KeyEvent.VK_V);
+            robot.delay(DELAY);
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.keyRelease(KeyEvent.VK_ENTER);
         } catch (AWTException e) {
